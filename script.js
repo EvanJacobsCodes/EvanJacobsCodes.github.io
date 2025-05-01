1b5595f73e4927ccad79bc036cc38b6934e179cd
EvanJacobsCodes <ejacobs0105@gmail.com>
2025-05-01 03:05:34 -0400

    123

---
+++ script.js
@@ -0,0 +1,105 @@
+function toggleNote(button) {
+    const note = button.nextElementSibling;
+    note.classList.toggle('hidden');
+    note.classList.toggle('visible');
+  }
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+
+  function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
+
+slide_Index = 1;
+show_Slides(slide_Index);
+
+// Next/previous controls
+function plus_Slides(n) {
+  show_Slides(slide_Index += n);
+}
+
+// Thumbnail image controls
+function current_Slide(n) {
+  show_Slides(slide_Index = n);
+}
+
+function show_Slides(n) {
+  let i;
+  let slides = document.getElementsByClassName("MySlides");
+  let dots = document.getElementsByClassName("Demo");
+  let captionText = document.getElementById("Caption");
+  if (n > slides.length) {slide_Index = 1}
+  if (n < 1) {slide_Index = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slide_Index-1].style.display = "block";
+  dots[slide_Index-1].className += " active";
+  captionText.innerHTML = dots[slide_Index-1].alt;
+}
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+  }
+}
